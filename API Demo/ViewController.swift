fbe8d20f7922b3328d3b933550aa36df9a1595c9
jmieville <jm.mieville@gmail.com>
2017-02-01 22:18:37 +0700

    Removed unused code and ported some changes into the code

--- API Demo/ViewController.swift
+++ API Demo/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cityInputName: String = ""
+    var buttonPressed = false
 
+    @IBOutlet weak var degreeCelsius: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var weatherCondition: UILabel!
+    @IBOutlet weak var cityInputBox: UITextField!
+    
+    @IBAction func submitCityName(_ sender: AnyObject) {
+        cityInputName = cityInputBox.text!.replacingOccurrences(of: " ", with: "")
+        changeCityWeather()
+        print(cityInputName)
+        cityInputBox.endEditing(true)
+    }
+    func changeCityWeather() {
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityInputName),uk&units=metric&appid=af01cc0475f79e722f443f915e1fe4bc")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print("error")
+            } else {
+                if let urlContent = data {
+                    do {
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.allowFragments)
+                        let cityName = ((jsonResult) as AnyObject)["name"]
+                        let weather = ((jsonResult as AnyObject)["weather"])
+                        let weatherDescription = (((((jsonResult as AnyObject)["weather"]) as! NSArray)[0] as? NSDictionary)?["description"] as? String)
+                        let mainDescription = ((jsonResult) as AnyObject)["main"]
+                        let tempMainDescriptionTest = (((mainDescription) as AnyObject)["temp"] as? String)
+                        DispatchQueue.main.async {
+                            self.city.text = cityName as! String?
+                            self.weatherCondition.text = weatherDescription as! String?
+                        }
+                    } catch {
+                        print("Json processing failed")
+                    }
+                }
+            }
+        }
+        task.resume()
+        print(task.currentRequest)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+     }
 }
 
